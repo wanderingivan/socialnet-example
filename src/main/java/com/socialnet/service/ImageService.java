74ebf5a5840ece66d327b2fc34dd76690600a356
shad <=>
2016-09-07 14:28:01 +0300

    Add imageService - interface that defines methods for manipulating dynamic images.
    Add ImageServiceImpl - implementation that delegates to ImageUtil and
    provides image caching.

---
+++ src/main/java/com/socialnet/service/ImageService.java
@@ -0,0 +1,48 @@
+package com.socialnet.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import com.socialnet.model.Image;
+/**
+ * 
+ * Service Layer interface providing
+ * basic save and retrieve operations
+ * for images.
+ * @throws IOExceptions when missing files
+ * or lacking credentials to save in directories 
+ */
+public interface ImageService  {
+	/**
+	 * Retrieve an image as a base 64 string
+	 * @param path of the file to use
+	 * @return an image converted to a base 64 string
+	 * @throws IOException
+	 */
+	String getB64(String path);
+	
+	byte [] loadImage(String path);
+	
+	/**
+	 * Saves an image to a predefined destination
+	 * @param file
+	 * @param contentType
+	 * @param fileName
+	 * @throws IOException
+	 */
+	String saveImage(File file,String contentType,String fileName) throws IOException;
+
+	/**
+	 * Remove and image from the default image folder
+	 * @param fileName
+	 * @throws IOException
+	 */
+	void removeImage(String fileName) throws IOException;
+
+	/**
+	 * Removes all images from list
+	 * @param userImages
+	 */
+	void removeImages(List<Image> userImages) throws IOException;
+}
---
+++ src/main/java/com/socialnet/service/impl/ImageServiceImpl.java
@@ -0,0 +1,103 @@
+package com.socialnet.service.impl;
+
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+
+import com.socialnet.model.Image;
+import com.socialnet.service.ImageService;
+import com.socialnet.util.ImageUtil;
+
+
+/** 
+ *	Implementation of ImageService
+ *	that adds caching for images 
+ *  and delegates image manipulation to ImageUtil
+ *  @see ImageService
+ *  @see  ImageUtil
+ *
+ */
+public class ImageServiceImpl implements ImageService {
+	
+
+	private String placeholderFilename;
+	private ImageUtil imageUtil;
+	
+	@Autowired
+	public ImageServiceImpl(ImageUtil imageUtil,String placeholderFilename){
+		super();
+		this.imageUtil = imageUtil;
+		this.placeholderFilename = placeholderFilename;
+	}
+	
+	/**
+	 * Uses image util to return an image converted as a b64 string.<br/>
+	 * Tries to resolve missing files by returning a placeholder
+	 * image
+	 */
+	@Override
+	@Cacheable(value="b64", key="#path")
+	public String getB64(String path){
+		try{
+			return imageUtil.encodeToB64String(path);
+		}catch(IOException missingFile){
+			System.out.println("Cant open file "+ path);
+			try{
+			    return imageUtil.encodeToB64String(placeholderFilename);
+			}catch(IOException ignore){
+				System.out.println("Cant open file "+ placeholderFilename );
+				throw new RuntimeException("Can't open placeholder file ");
+			}
+		}
+	}
+
+	/**
+	 * Uses image util to return an image loaded in a byte[].<br/>
+	 * Tries to resolve missing files by returning a placeholder
+	 * image
+	 */
+	@Override
+	@Cacheable(value="image", key="#path")
+	public byte[] loadImage(String path){
+		try{
+			return imageUtil.loadImage(path);
+		}catch(IOException missingFile){
+			System.out.println("Cant open file "+ path);
+			try{
+			    return imageUtil.loadImage(placeholderFilename);
+			}catch(IOException ignore){
+				System.out.println("Cant open file "+ placeholderFilename );
+				throw new RuntimeException("Can't open placeholder file ");
+			}
+		}
+	}
+
+
+
+	@Override
+	@CacheEvict(value="image",key="#path")
+	public void removeImage(String path) throws IOException {
+		File file = new File(path);
+		file.delete();
+	}
+
+	@Override
+	public void removeImages(List<Image> userImages) throws IOException {
+		for(Image i : userImages){
+			removeImage(i.getImagePath());
+		}
+	}
+	
+	@Override
+	public String saveImage(File file, String contentType, String fileName) throws IOException{
+		return imageUtil.saveImage(file, contentType, fileName);
+	}
+
+	
+}
