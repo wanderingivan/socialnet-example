01cfe79aa42a9d1fea0ae24a80737354dd0fef0a
shad <=>
2016-09-07 14:32:28 +0300

    Add AuthenticatedUserAware - interface for actions that require authenticated user info.
    Add AuthInterceptor - Interceptor that inserts authenticated users in
    actions that implement AuthenticatedUserAware interface.

---
+++ src/main/java/com/socialnet/interceptor/AuthInterceptor.java
@@ -0,0 +1,39 @@
+package com.socialnet.interceptor;
+
+import org.apache.log4j.Logger;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+public class AuthInterceptor extends AbstractInterceptor {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6165549166976305137L;
+	private static final Logger logger = Logger.getLogger(AuthInterceptor.class);
+
+	@Override
+	public String intercept(ActionInvocation invocation) throws Exception {
+		System.out.println("Auth Interceptor Intercepting action " + invocation.getAction().getClass().getSimpleName());
+		if(invocation.getAction() instanceof AuthenticatedUserAware){
+			if(logger.isDebugEnabled()){
+				logger.debug("Auth Interceptor Intercepting action " + invocation.getAction().getClass().getSimpleName() + "\n Inserting user" + getUserFromSecurityContext());
+			}
+			((AuthenticatedUserAware) invocation.getAction()).setUser(getUserFromSecurityContext());
+		}
+		return invocation.invoke();
+	}
+
+	private String getUserFromSecurityContext(){
+		
+		String username=  SecurityContextHolder.getContext()
+									       .getAuthentication()
+									       .getName();
+		if(username.equals("anonymousUser")){
+			return null;
+		}
+		return username;
+	}
+}
---
+++ src/main/java/com/socialnet/interceptor/AuthenticatedUserAware.java
@@ -0,0 +1,6 @@
+package com.socialnet.interceptor;
+
+public interface AuthenticatedUserAware {
+
+	public void setUser(String username);
+}
