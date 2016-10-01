f2ff717f3151550137ba7a7e074b0716e9bc167d
wanderingivan <angushev.ivan@gmail.com>
2016-10-01 18:11:47 +0300

    Add IncorrectPasswordException class

---
+++ src/main/java/com/socialnet/exception/IncorrectPasswordException.java
@@ -0,0 +1,10 @@
+package com.socialnet.exception;
+
+public class IncorrectPasswordException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8458344779315457810L;
+
+}
