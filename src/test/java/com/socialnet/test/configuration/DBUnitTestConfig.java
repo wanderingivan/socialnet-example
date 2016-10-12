452f4bf389e1ed44f5a83f3136ea6d4b74cb5ae7
wanderingivan <angushev.ivan@gmail.com>
2016-10-12 19:09:08 +0300

    Update test config

--- src/test/java/com/socialnet/test/configuration/DBUnitTestConfig.java
+++ src/test/java/com/socialnet/test/configuration/DBUnitTestConfig.java
@@ -21,9 +21,9 @@ public class DBUnitTestConfig {
 	public DataSource testDataSource(){
 		DriverManagerDataSource data = new DriverManagerDataSource();
 		data.setDriverClassName("com.mysql.jdbc.Driver");
-		data.setUrl("jdbc:mysql://localhost:3306/SOCIALNETTEST");
-		data.setUsername("tester");
-		data.setPassword("pass");
+		data.setUrl("jdbc:mysql://localhost:3306/SOCIALNETTEST?useSSL=false");
+		data.setUsername("root");
+		data.setPassword("password");
 		return data;
 	}
 
