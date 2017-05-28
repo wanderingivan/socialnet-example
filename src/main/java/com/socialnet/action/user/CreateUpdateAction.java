2b7038cd18c19f70a9cab91f581f8e9d34e508d8
wanderingivan <angushev.ivan@gmail.com>
2017-05-28 18:15:41 +0300

    Update CreateUpdateAction to include validations for password field.

--- src/main/java/com/socialnet/action/user/CreateUpdateAction.java
+++ src/main/java/com/socialnet/action/user/CreateUpdateAction.java
@@ -1,10 +1,13 @@
 package com.socialnet.action.user;
 
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.log4j.Logger;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.ParentPackage;
 import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.interceptor.ServletRequestAware;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.socialnet.exception.DuplicateEmailException;
@@ -17,9 +20,10 @@ import com.opensymphony.xwork2.validator.annotations.VisitorFieldValidator;
 
 
 @ParentPackage("user")
-public class CreateUpdateAction extends ActionSupport implements ModelDriven<User>{
+public class CreateUpdateAction extends ActionSupport implements ModelDriven<User>, ServletRequestAware {
 
 	
+    
 	
 	/**
 	 * 
@@ -30,14 +34,18 @@ public class CreateUpdateAction extends ActionSupport implements ModelDriven<Use
 	private UserService userService;
 	
 	private User user = new User();
+    private HttpServletRequest http;
 	
 
 	@Action(value="create",results={@Result(name="success", type="redirectAction", params={"actionName","show","username","${user.username}"}),
-								    @Result(name="input",location="/WEB-INF/content/jsp/base/create.jsp")})
+								    @Result(name="input", type="tiles", location="userCreateLayout")})
 	public String execute(){
 		try{
 	        if(logger.isInfoEnabled()){
 	            logger.info(String.format("Creating user with username %s and email %s",user.getUsername(),user.getEmail()));
+	        }
+	        if(!validatePassword(user.getPassword())){
+	            return INPUT;
 	        }
 			userService.createUser(user);
 			return SUCCESS;
@@ -59,21 +67,26 @@ public class CreateUpdateAction extends ActionSupport implements ModelDriven<Use
 		return ERROR;
 	}
 	
-	@Action(value="edit",results={@Result(name="success", type="redirectAction", params={"actionName","/logout"}),
-								  @Result(name="input",type="tiles", location="userEditLayout")})
+
+
+    @Action(value="edit",results={@Result(name="success", type="redirectAction", params={"actionName","/index","namespace","/"}),
+								  @Result(name="input",type="tiles", location="userEditInputLayout")})
 	public String editUser(){
 		try{
-			logger.info(String.format("Updating user with username %s and email %s",user.getUsername(),user.getEmail()));
+	        if(logger.isInfoEnabled()){
+			    logger.info(String.format("Updating user with id %d, username %s and email %s",user.getId(),user.getUsername(),user.getEmail()));
+	        }
 			userService.updateUser(user);
+	        http.logout();
 			return SUCCESS;
 			
 		}catch(DuplicateEmailException de){
-
+		    
 			addFieldError("user.email", getText("global.duplicate_email"));
 			return INPUT;
 
 		}catch(DuplicateUsernameException de){
-			
+		    
 			addFieldError("user.username", getText("global.duplicate_username"));
 			return INPUT;
 			
@@ -101,5 +114,27 @@ public class CreateUpdateAction extends ActionSupport implements ModelDriven<Use
 	public void setUserService(UserService service){
 		this.userService = service;
 	}
+
+    @Override
+    public void setServletRequest(HttpServletRequest http) {
+        this.http = http;
+    }
+    
+    /**
+     * Validates password string 
+     * and updates fieldErrors if needed
+     * @return
+     */
+	private boolean validatePassword(String password) {
+	    if(password.isEmpty()){ 
+	        addFieldError("user.password", getText("global.field_required"));
+	        return false;
+	    }
+	    if(password.length() > 50 || password.length() < 5){
+	        addFieldError("user.password", getText("global.field_password_length"));
+	        return false;
+	    }
+	    return true;
+    }
 
 }
