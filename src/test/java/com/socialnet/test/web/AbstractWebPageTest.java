5d71df22d99f31036190c39fa54054f07b36d44d
wanderingivan <angushev.ivan@gmail.com>
2017-05-29 19:55:25 +0300

    Add java doc to AbstractwebPageTest

--- src/test/java/com/socialnet/test/web/AbstractWebPageTest.java
+++ src/test/java/com/socialnet/test/web/AbstractWebPageTest.java
@@ -6,7 +6,11 @@ import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-
+/**
+ * Abstract class defining common 
+ * dependencies and methods for loading
+ * pages
+ */
 public class AbstractWebPageTest {
 
     @Drone
@@ -15,8 +19,11 @@ public class AbstractWebPageTest {
     @ArquillianResource
     protected URL deploymentUrl;
     
-    
-    
+    /**
+     * Appends the specified url to the deploymentUrl 
+     * and sets the window to 1920x1080 before attempting to load
+     * @param url the url to load
+     */   
     protected void loadPage(String url){
         driver.manage().window().setSize(new Dimension(1920,1080));
         driver.get(deploymentUrl.toString()
